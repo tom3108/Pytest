2a92992915a8097922c6d0cd999d0ef90ec88431
tom3108 <t.madziara@gmail.com>
2020-02-01 23:33:09 +0100

    pytest -v -s Print

--- test_math_func.py
+++ test_math_func.py
@@ -2,11 +2,12 @@ import math_func
 import pytest
 import sys
 
-@pytest.mark.skipif(sys.version_info > (3,3),reason="do not run number test")
+@pytest.mark.skipif(sys.version_info < (3,3),reason="do not run number test")
 def test_add():
     assert math_func.add(7,3) == 10
     assert math_func.add(7) == 9
     assert math_func.add(5) == 7
+    print('Test numer jeden')
 
 
 def test_product():
