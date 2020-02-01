167eac2ad1d3859611bd8b86282bce3ef2dacc9f
tom3108 <t.madziara@gmail.com>
2020-02-01 23:25:03 +0100

    Skipif

--- test_math_func.py
+++ test_math_func.py
@@ -1,7 +1,8 @@
 import math_func
 import pytest
+import sys
 
-@pytest.mark.skip(reason="do not run number test")
+@pytest.mark.skipif(sys.version_info > (3,3),reason="do not run number test")
 def test_add():
     assert math_func.add(7,3) == 10
     assert math_func.add(7) == 9
