5a6c92db226f04135b37abf70a64f81ce9adeaa2
tom3108 <t.madziara@gmail.com>
2020-02-01 23:19:31 +0100

    Skip decorator

--- test_math_func.py
+++ test_math_func.py
@@ -1,26 +1,26 @@
 import math_func
 import pytest
 
-@pytest.mark.number
+@pytest.mark.skip(reason="do not run number test")
 def test_add():
     assert math_func.add(7,3) == 10
     assert math_func.add(7) == 9
     assert math_func.add(5) == 7
 
-@pytest.mark.number
+
 def test_product():
     assert math_func.product(5, 5) == 25
     assert math_func.product(5) == 10
     assert math_func.product(7) == 14
 
-@pytest.mark.strings
+
 def test_add_strings():
     result=math_func.add('Hello',' World')
     assert result == 'Hello World'
     assert type(result) is  str
     assert 'XXX' not in result
 
-@pytest.mark.strings
+
 def test_product_strings():
     result = 'Hello Hello '
     assert math_func.product('Hello ') == result
