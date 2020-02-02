cb5202aee9e458d8d133c1321ba522027ff6a2e0
tom3108 <t.madziara@gmail.com>
2020-02-02 02:03:44 +0100

    Pytest fixture

--- test_math_func.py
+++ test_math_func.py
@@ -1,17 +1,20 @@
 from math_func import StudentDB
 import pytest
 
-def test_tom_data():
+@pytest.fixture(scope='module')
+def db():
+    print('-------setup------')
     db = StudentDB()
     db.connect('data.json')
+    return db
+
+def test_tom_data(db):
     tom_data = db.get_data("Tom")
     assert tom_data ['id'] == 1
     assert tom_data['name'] == "Tom"
     assert tom_data['result'] == "pass"
 
-def test_mac_data():
-    db = StudentDB()
-    db.connect('data.json')
+def test_mac_data(db):
     mac_data = db.get_data("Mac")
     assert mac_data ['id'] == 2
     assert mac_data['name'] == "Mac"
